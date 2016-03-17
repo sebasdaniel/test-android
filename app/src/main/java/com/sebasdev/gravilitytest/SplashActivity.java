a6fe04218fbc2c9a660f285a8f0ac2c45e19f1ff
sebasdaniel <sebasdaniel@github.com>
2016-03-16 20:45:25 -0500

    splash screen

---
+++ app/src/main/java/com/sebasdev/gravilitytest/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.sebasdev.gravilitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
