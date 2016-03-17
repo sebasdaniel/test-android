57f30f7eea57aa8a951bf52a3cb46cc121c430d4
sebasdaniel <sebasdaniel@github.com>
2016-03-16 23:32:04 -0500

    mejoramiento en splash screen, agregado toolbar y variables para fragments

--- app/src/main/java/com/sebasdev/gravilitytest/MainActivity.java
+++ app/src/main/java/com/sebasdev/gravilitytest/MainActivity.java
@@ -2,13 +2,28 @@ package com.sebasdev.gravilitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int FRAGMENT_CATEGORIES = 0;
+    public static final int FRAGMENT_APPS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setToolbar();
+    }
+
+    private void setToolbar() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.app_name);
     }
 }
